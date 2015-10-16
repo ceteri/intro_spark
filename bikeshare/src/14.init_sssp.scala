0e5f9be685fd96476c88f4b50dc0109bffcc0cb1
Paco Nathan <ceteri@gmail.com>
2015-10-16 09:47:58 +0200

    Update 14.init_sssp.scala

--- bikeshare/src/14.init_sssp.scala
+++ bikeshare/src/14.init_sssp.scala
@@ -3,5 +3,5 @@ sql("SELECT * FROM ranks WHERE station LIKE 'Columbus Circle%' ").show()
 sql("SELECT * FROM ranks WHERE station LIKE '8th%' ").show()
 
 // initialize for SSSP
-val sourceId: VertexId = 190
+val sourceId: VertexId = 194
 val initialGraph : Graph[(Double, List[VertexId]), Double] = g.mapVertices((id, _) => if (id == sourceId) (0.0, List[VertexId](sourceId)) else (Double.PositiveInfinity, List[VertexId]()))
