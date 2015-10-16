371ce28b1dd07a6a68013042b9fd601e33f646e5
Paco Nathan <ceteri@gmail.com>
2015-10-16 09:49:35 +0200

    Update 16.map_routes.scala

--- bikeshare/src/16.map_routes.scala
+++ bikeshare/src/16.map_routes.scala
@@ -1,7 +1,7 @@
 sssp.vertices.collect.foreach(println)
 
 // to confirm about Google Maps estimates
-sssp.vertices.filter(_._1 == 274).collect()
+sssp.vertices.filter(_._1 == 278).collect()
 225/60.
 
 sql("SELECT * FROM ranks WHERE station LIKE 'Lincoln%' ").show()
